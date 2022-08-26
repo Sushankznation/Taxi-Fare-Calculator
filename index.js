e33218fabf0092a21ce8347f259443b39f94be29
Sushankznation <sushantbharwal@gmail.com>
2022-08-26 20:25:30 +0530

    changes

--- index.js
+++ index.js
@@ -4,29 +4,109 @@
   }
   //For Calculate Fare
   function calculate(){
-    let price = document.getElementById('price');
-    let totalRoute = document.getElementById('from').value;
+    let totalRoute = document.getElementById('kilo').value;
     parseInt(totalRoute)
-    let display = document.getElementById('display');
-    let RateReason = document.getElementById('price');
-    RateReason.innerHTML = "Rs. 5/km for the first 10km, Rs2/km for the next 20, and Rs1/km after that";
+ let RateReason = document.getElementById('price');
+    // RateReason.innerHTML = "Rs. 5/km for the first 10km, Rs2/km for the next 20, and Rs1/km after that";
     let kmFare = 1;
     if(totalRoute <=0)
     return;
     else if(totalRoute <=10){
       kmFare = 5;
     }
-    else if(totalRoute <=20){
+     else if(totalRoute <=20){
       kmFare = 2;
     }
     else{
       kmFare = 1;
     }
     let totalFare = (totalRoute*kmFare);
-    display.innerHTML =totalFare +" Rs.";
+    // display.innerHTML = totalFare +" Rs.";
+    let display = document.getElementById('display').innerHTML = totalFare+"Rs.";
+    
+  
   }
-  // to fetch Google Map
-   let input;
-      function onOpenHandler(){
-         input = window.open('https://www.google.com/maps/dir//Chandigarh,+Punjab/@30.5390636,75.9200133,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x391069a660696aef:0x3bc789e57615106b!2m2!1d75.9550329!2d30.5389944','_blank','width=800px,height=400px,left=100px,top=100px');
+  // API-HERO
+  $(function () {
+    // add input listeners
+    google.maps.event.addDomListener(window, "load", function () {
+      var from_places = new google.maps.places.Autocomplete(
+        document.getElementById("from_places")
+      );
+      var to_places = new google.maps.places.Autocomplete(
+        document.getElementById("to_places")
+      );
+
+      google.maps.event.addListener(
+        from_places,
+        "place_changed",
+        function () {
+          var from_place = from_places.getPlace();
+          var from_address = from_place.formatted_address;
+          $("#origin").val(from_address);
+        }
+      );
+
+      google.maps.event.addListener(
+        to_places,
+        "place_changed",
+        function () {
+          var to_place = to_places.getPlace();
+          var to_address = to_place.formatted_address;
+          $("#destination").val(to_address);
+        }
+      );
+    });
+    // calculate distance
+    function calculateDistance() {
+      var origin = $("#origin").val();
+      var destination = $("#destination").val();
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [origin],
+          destinations: [destination],
+          travelMode: google.maps.TravelMode.DRIVING,
+          unitSystem: google.maps.UnitSystem.IMPERIAL, // miles and feet.
+          // unitSystem: google.maps.UnitSystem.metric, // kilometers and meters.
+          avoidHighways: false,
+          avoidTolls: false,
+        },
+        callback
+      );
+    }
+    // get distance results
+    function callback(response, status) {
+      if (status != google.maps.DistanceMatrixStatus.OK) {
+        $("#result").html(err);
+      } else {
+        var origin = response.originAddresses[0];
+        console.log(origin);
+        var destination = response.destinationAddresses[0];
+        console.log(destination);
+        if (response.rows[0].elements[0].status === "ZERO_RESULTS") {
+          $("#kilo").html(
+            "Get Flight");
+        } else {
+          var distance = response.rows[0].elements[0].distance;
+          console.log(distance);
+          var duration = response.rows[0].elements[0].duration;
+          console.log(duration);
+          console.log(response.rows[0].elements[0].distance);
+          var distance_in_kilo = distance.value / 1000; // the kilom
+          var distance_in_mile = distance.value / 1609.34; // the mile
+          console.log(distance_in_kilo);
+          console.log(distance_in_mile);
+          $("#kilo").html(
+            `${distance_in_kilo.toFixed(2)}`
+            // Distance in Kilometre: 
+          );
+        }
       }
+    }
+    // print results on submit the form
+    $("#distance_form").submit(function (e) {
+      e.preventDefault();
+      calculateDistance();
+    });
+  });
