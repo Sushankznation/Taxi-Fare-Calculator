8ed386ad5a66f972bc4d093727c95a9a91ea8cd3
Sushant Bherwal <sushantbharwal@gmail.com>
2023-08-29 17:09:30 +0530

    done

--- new.js
+++ new.js
@@ -43,20 +43,18 @@ function openRegister() {
     var to_places = new google.maps.places.Autocomplete(
       document.getElementById("to_places")
     );
-
+  
     google.maps.event.addListener(from_places, "place_changed", function () {
       var from_place = from_places.getPlace();
       var from_address = from_place.formatted_address;
       $("#origin").val(from_address);
     });
-
+  
     google.maps.event.addListener(to_places, "place_changed", function () {
       var to_place = to_places.getPlace();
       var to_address = to_place.formatted_address;
       $("#destination").val(to_address);
     });
-  }
-
   // Calculate distance and display results
   function calculateDistance() {
     var origin = $("#origin").val();
